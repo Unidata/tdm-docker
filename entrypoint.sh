d13159a2ab819cabf4a8582901a10166b59bfcc5
Julien Chastang <julien.c.chastang@gmail.com>
2024-07-29 15:33:36 -0600

    Gracefully handle cases where the selected USER_ID and/or GROUP_ID exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,15 @@ if [ "$1" = 'tdm.sh' ]; then
     ###
     # Tomcat user
     ###
+    if ! getent group $GROUP_ID &> /dev/null; then
+      groupadd -r tomcat -g $GROUP_ID
+    fi
+    # create user for USER_ID if one doesn't already exist
+    if ! getent passwd $USER_ID &> /dev/null; then
+      useradd -u $USER_ID -g $GROUP_ID tomcat
+    fi
+    # alter USER_ID with nologin shell and CATALINA_HOME home directory
+    usermod -d "${CATALINA_HOME}" -s /sbin/nologin $(id -u -n $USER_ID)
     groupadd -r tomcat -g ${GROUP_ID} && \
     useradd -u ${USER_ID} -g tomcat -d ${CATALINA_HOME} -s /sbin/nologin \
         -c "Tomcat user" tomcat
@@ -18,9 +27,9 @@ if [ "$1" = 'tdm.sh' ]; then
     # Restrict permissions on conf
     ###
 
-    chown -R tomcat:tomcat ${TDM_HOME}
+    chown -R $USER_ID:$GROUP_ID ${TDM_HOME}
     sync
-    exec gosu tomcat "$@"
+    exec gosu $USER_ID "$@"
 fi
 
 exec "$@"
